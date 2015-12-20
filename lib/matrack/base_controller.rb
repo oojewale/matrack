fcb99c6ca52bd21bf328f5040e31a6f15e5b845a
Olaide Ojewale <ojewaleolaide@gmail.com>
2015-12-20 11:40:15 +0100

    Implement routing and view rendering.

--- lib/matrack/base_controller.rb
+++ lib/matrack/base_controller.rb
@@ -2,8 +2,6 @@ module Matrack
   class BaseController
     attr_reader :request
     def initialize env
-      # @env = env
-      # @response = Response.new
       @request = Rack::Request.new env
     end
 
@@ -11,30 +9,23 @@ module Matrack
       request.params
     end
 
-    # def render(*args)
-    #   args << controller_name
-    #   @response_obj = @response.rack_response(@response.render_template(*args))
-    #     @response_obj.body
-    # end
-
     def render(view_name, locals = {})
-      unless locals.empty?
-        render_template(view_name, locals)
-      else
-        render_template(view_name)
-      end
-    end
-
-    def render_template(view_name, locals = {})
-      template = Tilt::ERBTemplate.new(File.join(File.dirname(__FILE__), "layout.erb"))
+      template = Tilt::ERBTemplate.new(File.join("app", "views", "layout", "application.html.erb"))
       title = view_name.to_s.gsub("_", " ").capitalize
-      template.render(self, title: title) { Tilt::ERBTemplate.new(File.join("app", "views", controller_name, "#{view_name}.erb")).render(self, locals)}
+      template.render(self, title: title) { Tilt::ERBTemplate.new(File.join("app", "views", controller_name, "#{view_name}.erb")).render(self, locals.merge!(get_instance_vars))}
     end
 
+    def get_instance_vars
+      vars = {}
+      instance_variables.each do | var |
+        key = var.to_s.gsub("@","").to_sym
+        vars[key] = instance_variable_get(var)
+      end
+      vars
+    end
 
     def controller_name
       self.class.to_s.gsub(/Controller$/, "").to_snake_case
     end
-
   end
 end
--- spec/matrack_spec.rb
+++ spec/matrack_spec.rb
@@ -5,7 +5,7 @@ describe Matrack do
     expect(Matrack::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
+  # it 'does something useful' do
+  #   expect(false).to eq(true)
+  # end
 end
